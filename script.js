8ca051efb10cf9dc661d3879b6576dc8ee9f216f
Saumya Saxena <saxenasaumya027@gmail.com>
2025-04-25 14:12:54 +0530

    Initial portfolio upload

---
+++ script.js
@@ -0,0 +1,11 @@
+document.getElementsByClassName("email").addEventListener("click",()=>{
+        window.open("mailto:saxenasaumya027@gmail.com","_blank");
+})
+document.getElementsByClassName("linkedin").addEventListener("click",()=>{
+    window.open("https://www.linkedin.com/in/saumya-saxena-900798213","_blank");
+})
+document.getElementsByClassName("github").addEventListener("click",()=>{
+    window.open("https://github.com/AKA-027","_blank");
+})
+
+
